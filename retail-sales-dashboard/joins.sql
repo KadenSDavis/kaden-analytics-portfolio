90ab6698ffdb8fd37e072f33f3ff255ba734ee1a
KadenSDavis <kadendavis338@gmail.com>
2026-09-17 21:44:31 -0400

    Update joins.sql

--- retail-sales-dashboard/joins.sql
+++ retail-sales-dashboard/joins.sql
@@ -1,1 +1,13 @@
+SELECT
+    r.Order_ID,
+    r.Order_Date,
+    r.Region,
+    r.Category,
+    r.Sub_Category,
+    r.Sales,
+    r.Profit,
+    c.Customer_Name
+FROM retail_sales r
+LEFT JOIN customers c
+    ON r.Customer_ID = c.Customer_ID;
 
