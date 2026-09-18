76a0a4e787afa8e84675e8e5b6f20d941d6bae71
KadenSDavis <kadendavis338@gmail.com>
2026-09-17 22:50:47 -0400

    Delete retail-sales-dashboard directory

--- retail-sales-dashboard/window_functions.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    Region,
-    Category,
-    Sales,
-    SUM(Sales) OVER (PARTITION BY Region) AS Regional_Sales,
-    RANK() OVER (PARTITION BY Region ORDER BY Sales DESC) AS Regional_Rank
-FROM retail_sales;
-
